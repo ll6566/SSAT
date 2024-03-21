a0abc669dc74772d5e8b8f1bec02d2b03dd454e0
zhanglianwu <18974710587@163.com>
2024-03-21 14:44:02 +0800

    v2.2.2

--- bin/run.bat
+++ bin/run.bat
@@ -1,10 +1,9 @@
 @echo off
 cd /d %~dp0
 cd ..
-if exist "venv" (
-	echo "已经安装好python虚拟环境"
-) else (
+if not exist "venv" (
 	echo "请先执行 'envir_config.bat' 安装配置Python虚拟环境 "
+	cmd
 )
 set pythonExe=%cd%\venv\Scripts\python.exe
 %pythonExe% %cd%\src\main.py
